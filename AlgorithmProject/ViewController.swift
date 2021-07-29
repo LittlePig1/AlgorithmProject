d01a5fc3fc8fd0113a62a7e4373ec0186259a887
王金志 <wangjinzhi@aerozhonghuan.com>
2021-07-29 18:11:44 +0800

    提交

--- AlgorithmProject/ViewController.swift
+++ AlgorithmProject/ViewController.swift
@@ -28,9 +28,17 @@ class ViewController: UIViewController {
         var num2 = [44,-7,-3,2,3,11,7,0]
         print("平方后的新数组 方法1 \(getNewArray(num: &num2))")
         
-        let num3 = [44,-7,-3,2,3,11,7,0]
+        let num3 = [-7,-3,2,3,7,11]
         print("平方后的新数组 方法2 \(getNewArrayTwo(num: num3))")
         
+        let num4 = [2,3,1,2,4,3]
+        let s = 7
+        print("长度最小的子数组1 \(getLessThanArray(num: num4, target: s))")
+        print("长度最小的子数组2 \(getLessThanArrayTwo(num: num4, target: s))")
+
+        let s1 = 3
+        print("矩阵数组 \(generateMatrix(target: s1))")
+
     }
     // MARK:- 数组
     /*
@@ -123,7 +131,7 @@ class ViewController: UIViewController {
         // 快速排序
         return qucikSort(&num, 0, num.count - 1)
     }
-    // 快速排序
+    // 快速排序 *****特别关注******
     public func qucikSort(_ array: inout [Int], _ p: Int, _ r: Int) -> [Int] {
         // 需要排序的区间只包含一个数字，则不需要重排数组，直接返回
         if p >= r { return array }
@@ -153,25 +161,113 @@ class ViewController: UIViewController {
     // 双指针
     fileprivate func getNewArrayTwo(num: [Int]) -> [Int] {
         let size = num.count
-        var newNum = Array<Int?>(repeating: nil, count: size)
+        var newNum = Array<Int?>(repeating: 0, count: size)
 
         var left = 0
         var right = size - 1
-        
+        var temRight = size - 1
+
         while left <= right {
             if num[left]*num[left] < num[right]*num[right] {
-                newNum[right] = num[right]*num[right]
+                newNum[temRight] = num[right]*num[right]
                 right -= 1
             } else {
-                newNum[right] = num[left]*num[left]
+                newNum[temRight] = num[left]*num[left]
                 left += 1
             }
+            temRight -= 1
         }
         if let resultNum = newNum as? [Int] {
             return resultNum
         } else {
             return num
         }
+    }
+    /*
+     给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+     示例：
+     输入：s = 7, nums = [2,3,1,2,4,3] 输出：2 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+     */
+    // 方法一：
+    fileprivate func getLessThanArray(num: [Int], target: Int) ->Int {
+        let numMaxIndex = num.count - 1
+        var length1 = 0
+        var length2 = num.count
+        for i in 0...numMaxIndex {
+            var sum = 0
+            for j in i...numMaxIndex {
+                sum += num[j]
+                if sum >= target {
+                    length1 = j - i + 1
+                    length2 = length2 > length1 ? length1: length2
+                    break
+                }
+            }
+        }
+        return length2
+    }
+    // 方法二： 滑动窗口
+    fileprivate func getLessThanArrayTwo(num: [Int], target: Int) ->Int {
+        var left = 0
+        var length1 = num.count
+        var length2 = 0
+        var sum = 0
+        
+        for i in 0...(length1 - 1) {
+            sum += num[i]
+            while sum >= target {
+                length2 = i - left + 1
+                length1 = length2 > length1 ? length1 : length2
+                sum -= num[left]
+                left += 1
+            }
+        }
+        return length1
+    }
+    /*
+     给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵
+     示例:
+     输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
+      ——————
+     ｜1 2 3｜
+     ｜8 9 4｜
+     ｜7 6 5｜
+      ——————
+     */
+    // 左闭右开
+    fileprivate func generateMatrix(target: Int) -> [[Int]] {
+        var num = [[Int]]()
+        var startX = 0
+        var startY = 0
+        let count = target/2
+        // 偏移量
+        var offSet = 1
+        var a = 0
+        
+        while count > 0 {
+            var temi = startX
+            var temj = startY
+            // 上面一行
+            for i in startY...(startY+target-offSet)  {
+                a += 1
+                temj = i
+                num[startX][temj] = a
+            }
+            // 右边一列
+            for i in startX...(startX+target-offSet)  {
+                a += 1
+                temi = i
+                num[temi][temj] = a
+            }
+            // 下面一行
+            for i in startY...(startY+target-offSet)  {
+                a += 1
+                temj = i
+                num[temi][temj] = a
+            }
+        }
+        
+        return num
     }
 }
 
