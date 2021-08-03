c6b57b07352a1cdbf496ce37e05f66fb415d4cd0
王金志 <wangjinzhi@aerozhonghuan.com>
2021-08-03 18:06:52 +0800

    链表

--- AlgorithmProject/Page/LB.swift
+++ AlgorithmProject/Page/LB.swift
@@ -13,7 +13,27 @@ class LB: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         
-        
+        // 删除链表中等于给定值 val 的所有节点
+        var node0 = Node(6)
+        let node1 = Node(2)
+        let node2 = Node(6)
+        let node3 = Node(3)
+        let node4 = Node(4)
+        let node5 = Node(5)
+        let node6 = Node(6)
+
+        node0.next = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+        node5.next = node6
+
+//        removeTargat1(6, headNode: &node0)
+//        removeTargat2(6, headNode: &node0)
+
+        // 反转一个单链表
+        print("反转一个单链表---\(reverseList(headNode: &node0))")
     }
     /*
      题：删除链表中等于给定值 val 的所有节点。
@@ -30,15 +50,79 @@ class LB: UIViewController {
      输入：head = [7,7,7,7], val = 7
      输出：[]
      */
-    fileprivate func removeTargat() {
+    // 常规方法
+    fileprivate func removeTargat1(_ target: Int, headNode: inout Node) {
+        // 头节点 处理
+        while headNode.val == target {
+            if let node = headNode.next {
+                headNode = node
+            }
+        }
+        // 非头节点 处理
+        var currentNode = headNode
+        while currentNode.next != nil {
+            if currentNode.next?.val == target {
+                currentNode.next = currentNode.next?.next
+            } else {
+                if let node = currentNode.next {
+                    currentNode = node
+                }
+            }
+        }
         
+        print("删除链表中等于给定值---\(headNode)")
     }
+    // 虚拟头节点
+    fileprivate func removeTargat2(_ target: Int, headNode: inout Node) {
+        let temHeaderNode = Node(0)
+        temHeaderNode.next = headNode
+        
+        var currentNode = temHeaderNode
+        while currentNode.next != nil {
+            if currentNode.next?.val == target {
+                currentNode.next = currentNode.next?.next
+            } else {
+                if let node = currentNode.next {
+                    currentNode = node
+                }
+            }
+        }
+        if let node = temHeaderNode.next {
+            headNode = node
+        }
+        print("删除链表中等于给定值---\(headNode)")
+    }
+    /*
+     题意：反转一个单链表。
 
+     示例: 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
+     */
+    // 双指针
+    fileprivate func reverseList(headNode: inout Node) -> Node{
+        var cur = headNode
+        var pre: Node?
+        var tem: Node?
+        while cur.next != nil {
+            tem = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tem!
+        }
+        if cur.next == nil {
+            tem = cur.next
+            cur.next = pre
+            pre = cur
+            if tem != nil {
+                cur = tem!
+            }
+        }
+        return pre!
+    }
 }
 // 链表节点
-class ListNode {
+class Node {
   var val: Int
-  var next: ListNode?
+  var next: Node?
   
   init(_ val: Int) {
     self.val = val
